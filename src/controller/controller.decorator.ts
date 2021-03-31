84a871822fc8dce3af40f825e670d8c4c480f2ed
Mario Sabo <mariosabo2@gmail.com>
2021-03-31 18:17:50 +0200

    (refactor)[controller]: move magic string to constants file

--- src/controller/controller.decorator.ts
+++ src/controller/controller.decorator.ts
@@ -1,7 +1,8 @@
 import { Constructible } from '../common/types/constructible.type';
+import { ControllerMetadataKey } from './controller.constants';
 
 export function Controller(route: string) {
   return function (target: Constructible) {
-    Reflect.defineMetadata('route', route, target);
+    Reflect.defineMetadata(ControllerMetadataKey.ROUTE, route, target);
   };
 }
