6cb5de49374637e407a8e563e51695efe09e4999
Mario Sabo <mariosabo2@gmail.com>
2021-04-28 18:08:57 +0200

    (refactor): add barrel files

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './app';
+export * from './controller';
+export * from './dependency-injection';
+export * from './http-methods';
+export * from './logger';
+export * from './method-arguments';
+export * from './middleware';
---
+++ src/middleware/index.ts
@@ -0,0 +1,1 @@
+export * from './use-middlewares.decorator';
