3f8e523dea4950ab51486d7ec29405174a1d1789
Mario Sabo <mariosabo2@gmail.com>
2021-05-02 12:02:02 +0200

    (feat)[controllers][default-http-status]: change decorator param name to code as it makes more sense

--- src/controllers/default-http-status/default-http-status.decorator.ts
+++ src/controllers/default-http-status/default-http-status.decorator.ts
@@ -1,11 +1,11 @@
 import { DefaultHttpStatusMetadataKey } from './default-http-status.constants';
 
-export function DefaultHttpStatus(status: number): ClassDecorator & MethodDecorator {
+export function DefaultHttpStatus(code: number): ClassDecorator & MethodDecorator {
   return function (target: any, key?: string | symbol) {
     if (key) {
-      Reflect.defineMetadata(DefaultHttpStatusMetadataKey.DEFAULT_HTTP_STATUS, status, target, key);
+      Reflect.defineMetadata(DefaultHttpStatusMetadataKey.DEFAULT_HTTP_STATUS, code, target, key);
     } else {
-      Reflect.defineMetadata(DefaultHttpStatusMetadataKey.DEFAULT_HTTP_STATUS, status, target);
+      Reflect.defineMetadata(DefaultHttpStatusMetadataKey.DEFAULT_HTTP_STATUS, code, target);
     }
   };
 }
